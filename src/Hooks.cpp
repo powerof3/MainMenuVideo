4bc1d29ea3c6a3d5a5745f298dcc767ad8fcb2cc
powerof3 <powerof3@users.noreply.github.com>
2025-10-03 05:08:55 +0530

    Fix 1.6.640 crash

--- src/Hooks.cpp
+++ src/Hooks.cpp
@@ -4,6 +4,29 @@
 
 namespace Hooks
 {
+	struct detail
+	{
+		static const char* GetGameVersionImpl()
+		{
+			using func_t = decltype(&GetGameVersionImpl);
+			static REL::Relocation<func_t> func{ RELOCATION_ID(15485, 15650) };
+			return func();
+		}
+
+		static REL::Version GetGameVersion()
+		{
+			std::stringstream            ss(GetGameVersionImpl());
+			std::string                  token;
+			std::array<std::uint16_t, 4> version{};
+
+			for (std::size_t i = 0; i < 4 && std::getline(ss, token, '.'); ++i) {
+				version[i] = static_cast<std::uint16_t>(std::stoi(token));
+			}
+
+			return REL::Version(version);
+		}
+	};
+
 	struct TriggerMainMenuMusic
 	{
 		static bool thunk()
@@ -19,8 +42,13 @@ namespace Hooks
 
 	void Install()
 	{
+		const auto gameVersion = detail::GetGameVersion();
+
 		std::array targets{
-			std::make_pair(RELOCATION_ID(51238, 52110), OFFSET(0x19, 0x2A)),
+			std::make_pair(RELOCATION_ID(51238, 52110), (gameVersion >= SKSE::RUNTIME_SSE_1_6_629 && gameVersion <= SKSE::RUNTIME_SSE_1_6_640 ?
+																0x22 :
+																OFFSET(0x19, 0x2A))),
+
 			std::make_pair(RELOCATION_ID(51259, 52137), OFFSET(0xFD, 0x2DD)),
 		};
 
