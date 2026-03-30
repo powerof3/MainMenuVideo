e0492fd8aa3a3473dea8b533ef80894fae7323ca
doodlum <15017472+doodlum@users.noreply.github.com>
2026-03-30 17:53:14 +0100

    chore: cleanup

--- src/PCH.h
+++ src/PCH.h
@@ -6,20 +6,8 @@
 #include "RE/Skyrim.h"
 #include "SKSE/SKSE.h"
 
-#include <spdlog/sinks/basic_file_sink.h>
-#include <xbyak/xbyak.h>
-
 #define DLLEXPORT __declspec(dllexport)
 
 using namespace std::literals;
-
-namespace logger = SKSE::log;
-
-using EventResult = RE::BSEventNotifyControl;
-
-namespace stl
-{
-	using namespace SKSE::stl;
-}
 
 #include "Version.h"
